4a80c96708123f72ca743e42070a1086d90883b0
Vidhisha Balachandran <vbalacha@marten.ml.cmu.edu>
2019-11-24 00:08:04 -0500

    Script changes

--- chain_extractor_cnndm/gen_labels.sh
+++ chain_extractor_cnndm/gen_labels.sh
@@ -6,7 +6,7 @@ do
         do
         chunk=$(($i+$j))
         nohup python3 preprocess_generate_labels.py \
-              --input_dir=./cnn_stories_tokenized_chunked/chunk_${chunk}000 \
+              --input_dir=../cnn_stories_tokenized_chunked/chunk_${chunk}000 \
               --ner_output_dir=../cnn_stories_ner_heuristic_chain_labels \
               --contsel_output_dir=../cnn_stories_contsel_tags_labels  > log/cnn_${chunk}.log 2>&1 &
         echo $chunk
@@ -14,3 +14,8 @@ do
         wait
         echo "All Done for this iteration"
 done
+
+#python3 preprocess_generate_labels.py \
+#              --input_dir=../cnn_stories_tokenized_chunked/chunk_1000 \
+#              --ner_output_dir=../cnn_stories_ner_coref_heuristic_chain_labels \
+#              --contsel_output_dir=../cnn_stories_contsel_tags_labels
